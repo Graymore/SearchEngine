800ff8c5c4dde09c162957e0640d977a1682003a
Dmitry <voult@list.ru>
2023-08-30 22:13:08 +0400

    update

--- src/SearchServiceProvider.php
+++ src/SearchServiceProvider.php
@@ -6,16 +6,18 @@ use Illuminate\Support\ServiceProvider;
 
 class SearchServiceProvider extends ServiceProvider
 {
+
+    public function register(): void
+    {
+        $this->commands([
+            \SearchEngine\Shell::class
+        ]);
+    }
+
     public function boot(): void
     {
         $this->publishes([
             __DIR__.'/../config/search.php' => config_path('search.php'),
         ]);
-
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                \SearchEngine\Shell::class
-            ]);
-        }
     }
 }
